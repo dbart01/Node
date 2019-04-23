d322cef66ecf4734e914f5be1928d1f287035e2c
Dima Bart <dima.bart01@gmail.com>
2019-04-23 15:44:29 -0400

    Initial Commit

---
+++ Node/Node.h
@@ -0,0 +1,19 @@
+//
+//  Node.h
+//  Node
+//
+//  Created by Dima Bart on 2019-04-23.
+//  Copyright © 2019 Dima Bart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Node.
+FOUNDATION_EXPORT double NodeVersionNumber;
+
+//! Project version string for Node.
+FOUNDATION_EXPORT const unsigned char NodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Node/PublicHeader.h>
+
+
---
+++ NodeTests/NodeTests.swift
@@ -0,0 +1,34 @@
+//
+//  NodeTests.swift
+//  NodeTests
+//
+//  Created by Dima Bart on 2019-04-23.
+//  Copyright © 2019 Dima Bart. All rights reserved.
+//
+
+import XCTest
+@testable import Node
+
+class NodeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
